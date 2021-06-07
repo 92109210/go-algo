437f7b3f4fe13615465b65b40f49d6c23011b56a
fx <fx@123.com>
2021-06-07 09:18:22 +0800

    no message

--- tree/116/main.go
+++ tree/116/main.go
@@ -1,5 +1,26 @@
 package main
 
+type Node struct {
+	Val   int
+	Left  *Node
+	Right *Node
+	Next  *Node
+}
+
 func main() {
 
+}
+
+func connect(root *Node) *Node {
+	if root == nil || root.Left == nil {
+		return root
+	}
+
+	root.Left.Next = root.Right
+	if root.Next != nil {
+		root.Right.Next = root.Next.Left
+	}
+	connect(root.Left)
+	connect(root.Right)
+	return root
 }
